f6f1139c984d54126ba6186f22998d232f98d0ab
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-12 11:06:29 -0500

    Initial Commit

---
+++ NameMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NameMe
+//
+//  Created by Antonio Vega on 8/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NameMe/NameMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameMeApp.swift
+//  NameMe
+//
+//  Created by Antonio Vega on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NameMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
